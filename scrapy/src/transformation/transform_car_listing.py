7209b79059b260421f4c75183146cc8e102957ff
tmsnobrega <tmsnobrega@gmail.com>
2025-02-10 18:28:27 +0100

    feat: adding more business rules

--- scrapy/src/transformation/transform_car_listing.py
+++ scrapy/src/transformation/transform_car_listing.py
@@ -134,6 +134,7 @@ def extract_equipment_features(df):
 
 ### Apply transformations ###
 def transform_data():
+  print("Initiating data transformation...")
   with open(RAW_FILE_PATH, "r", encoding="utf-8") as file:
     try:
       data = [json.loads(line) for line in file]
@@ -141,6 +142,8 @@ def transform_data():
       print(f"Skipping malformed JSON line: {e}")
       return data
   df = pd.DataFrame(data)
+
+  print(f"\tRows before any filters: {len(df)}")
 
   # Call functions
   df["manufacturer"] = df["manufacturer"].apply(clean_text)
@@ -162,6 +165,13 @@ def transform_data():
 
   ### Other transformations and business rules ###
 
+  # Establish min and max thresholds for numerical columns
+  df["empty_weight_kg"] = df["empty_weight_kg"].apply(lambda x: x if 1_000 <= x <= 3_000 else None)
+  df["km"] = df["km"].apply(lambda x: x if 0 <= x <= 400_000 else None)
+  df["engine_power_hp"] = df["engine_power_hp"].apply(lambda x: x if 70 <= x <= 700 else None)  
+  df["engine_size_cc"] = df["engine_size_cc"].apply(lambda x: x if 600 <= x <= 8_000 else None)
+  df["co2_emission_g_per_km"] = df["co2_emission_g_per_km"].apply(lambda x: x if 0 <= x <= 300 else None)
+
   # Calculate car age in months
   df["car_age_in_months"] = df["built_in"].apply(lambda x: (datetime.now().year - x.year) * 12 + datetime.now().month - x.month if pd.notnull(x) else None)
   df["car_age_in_months"] = df["car_age_in_months"].replace(-1, 0)
@@ -171,29 +181,26 @@ def transform_data():
   df["years_active"] = df["active_since"].apply(lambda x: (datetime.now().year - x) if pd.notnull(x) else None)
 
   # Update gear type for eletric cars
-  df["gear_type"] = df.apply(lambda row: "Automatic" if row["fuel"] == "Electric" else row["gear_type"], axis=1)
+  df["gear_type"] = df.apply(lambda row: "Automatic" if row["fuel"] == "Electric" else row["gear_type"], axis=1) # If a car is eletric, it is automatic
 
   # Update number of previous owners & used_or_new
-  df["previous_owners"] = df.apply(lambda row: 0 if row["used_or_new"] == "New" else row["previous_owners"], axis=1)
-  df.loc[(df["car_age_in_months"] <= 12) & (df["km"] < 1_000), ["previous_owners", "used_or_new"]] = [0, "New"]
-  df["used_or_new"] = df["used_or_new"].apply(lambda x: "Used" if x != "New" else x)
+  df["previous_owners"] = df.apply(lambda row: 0 if row["used_or_new"] == "New" else row["previous_owners"], axis=1) # If a car is new, it has 0 previous owners
+  df.loc[(df["car_age_in_months"] <= 12) & (df["km"] < 1_000), ["previous_owners", "used_or_new"]] = [0, "New"] # If a car is lesnn than 12 months old and less then 1000 km, it is new and has 0 previous owners
+  df["used_or_new"] = df["used_or_new"].apply(lambda x: "Used" if x != "New" else x) # If a car is not new, it is used
   
-  # Update body type labels for SUVs
+  # Update drive train
+  df["drive_train"] = df.apply(lambda row: "4WD" if pd.isna(row["drive_train"]) and any(x in row["description"] for x in ["AWD", "4WD"]) else row["drive_train"], axis=1)
+
+  # Update labels
   df["body_type"] = df["body_type"].replace("Off-Road/Pick-up", "SUV")
+  df["fuel"] = df["fuel"].replace("Electric/Gasoline", "Hybrid")
 
-  # Update engine size thresholds
-  df["engine_size_cc"] = df["engine_size_cc"].apply(lambda x: x if 600 <= x <= 8_000 else None)
 
-  # Update co2 emission thresholds and co2 emission depeding on the fuel type
-  df["co2_emission_g_per_km"] = df["co2_emission_g_per_km"].apply(lambda x: x if 0 <= x <= 300 else None)
-  df["co2_emission_g_per_km"] = df.apply(lambda row: 0 if row["fuel"] == "Electric" else row["co2_emission_g_per_km"], axis=1)
-  df["co2_emission_g_per_km"] = df.apply(lambda row: None if row["co2_emission_g_per_km"] == 0 and row["fuel"] != "Electric" else row["co2_emission_g_per_km"], axis=1)
+  # Update co2 emission depeding on the fuel type
+  df["co2_emission_g_per_km"] = df.apply(lambda row: 0 if row["fuel"] == "Electric" else row["co2_emission_g_per_km"], axis=1) # If a car is eletric, it has 0 co2 emission
+  df["co2_emission_g_per_km"] = df.apply(lambda row: None if row["co2_emission_g_per_km"] == 0 and row["fuel"] != "Electric" else row["co2_emission_g_per_km"], axis=1) # If a car is not eletric and has 0 co2 emission, it is set to None
 
-  # Establish min and max thresholds
-  df["empty_weight_kg"] = df["empty_weight_kg"].apply(lambda x: x if 1_000 <= x <= 3_000 else None)
-  df["km"] = df["km"].apply(lambda x: x if 0 <= x <= 400_000 else None)
-  df["engine_power_hp"] = df["engine_power_hp"].apply(lambda x: x if 70 <= x <= 700 else None)  
-  
+
   ### Drop unnecessary data according to exploration/explore_car_listing.py ###
 
   # Drop irrelevant columns
@@ -211,16 +218,24 @@ def transform_data():
   df.drop(df[df["fuel"].isin(["Electric/Diesel", None, ""])].index, inplace=True)
   df.drop(df[df["emission_class"].isin(["Euro 4", "Euro 5", "Euro 6c"])].index, inplace=True)
   df.drop(df[(df["fuel"] == "Gasoline") & (df["electric_range"] > 0)].index, inplace=True)
-  df.drop(df[df["car"].isin(["UX 300h", "UX 300e"])].index, inplace=True)
+  df.drop(df[(df["fuel"] == "Electric") & (~df["manufacturer"].isin(["Kia", "Tesla", "Volvo"]))].index, inplace=True) # Only keep Tesla, Kia and Volvo electric cars as other manufacturers do not have electric cars
+  df.drop(df[(df["manufacturer"] == "Lynk & Co") & (df["fuel"] != "Hybrid")].index, inplace=True) # Only keep Lynk & Co hybrid cars
+  df.drop(df[(df["car"] == "Niro") & (df["fuel"] != "Hybrid")].index, inplace=True) # Only keep Kia Niro hybrid cars
+  df.drop(df[(df["manufacturer"] == "Toyota") & (df["fuel"] != "Hybrid")].index, inplace=True) # Only keep Toyota hybrid cars
+  df.drop(df[(df["fuel"] == "Diesel") & (df["car"] != "A3")].index, inplace=True) # Only keep Audi A3 Diesel cars as there are not enough data entries for other Diesel cars
+  df.drop(df[df["car"].isin(["UX 300h", "UX 300e"])].index, inplace=True) # Not enought data entries for these Lexus models
+
+  print(f"\tRows after applying filters: {len(df)}")
 
   # Drop duplicate rows
   df.sort_values(by="timestamp", inplace=True)
   df.drop_duplicates(subset=["km", "price", "car", "listing_url"], keep="last", inplace=True)
 
+  print(f"\tRows after dropping duplicates: {len(df)}")
 
   ### Save transformed data as CSV ###
   df.to_csv(os.path.join(TRANSFORMED_FOLDER_PATH, "transformed_car_listing.csv"), index=False)
-  print("Data transformation completed!")
+  print("\tData transformation completed!")
 
 # Run transformation
 if __name__ == "__main__":
